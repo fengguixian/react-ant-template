8021a49363cc8a8e9f893352f3a886c0aa02af4e
fengguixian <fengguixian-520@163.com>
2021-11-29 16:58:36 +0800

    完成Header显示个人头像，并实现退出登录的功能

--- src/pages/mainLayout/ContentHeader.js
+++ src/pages/mainLayout/ContentHeader.js
@@ -1,12 +1,48 @@
-import { Button, Layout } from "antd";
+import { Button, Layout, Avatar, Popover, Modal } from "antd";
 import { connect } from "react-redux"
 import './ContentHeader.css';
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 import {
     MenuFoldOutlined,
-    MenuUnfoldOutlined
+    MenuUnfoldOutlined,
 } from '@ant-design/icons';
 
 const { Header } = Layout;
+
+const UserInfoPopoverContent = (props) => {
+    const navigate = useNavigate();
+    const [isLogoutModalShow, setIsLogoutModalShow] = useState(false);
+
+    function logoutModalOK() {
+        setIsLogoutModalShow(false);
+        window.localStorage.setItem('token', '');
+        navigate('login');
+    }
+
+    function logoutModalCancel() {
+        setIsLogoutModalShow(false);
+    }
+
+    return (
+        <div>
+            <Button type='primary' danger onClick={() => {
+                setIsLogoutModalShow(true);
+            }}>Logout</Button>
+
+            <Modal
+            title="温馨提示"
+            visible={isLogoutModalShow}
+            onOk={logoutModalOK}
+            onCancel={logoutModalCancel}
+            okText="确认"
+            cancelText="取消"
+            >
+                <p>您确定需要退出登录吗？</p>
+            </Modal>
+        </div>
+    )
+}
 
 const ContentHeader = (props) => {
     return (
@@ -15,15 +51,24 @@ const ContentHeader = (props) => {
         style={
             {
                 background: 'white',
-                padding: '0px 10px'
+                padding: '0px 25px 0px 10px',
+                display: 'flex',
+                justifyContent: 'space-between'
             }
         }>
-            <Button type='text' onClick={props.foldHandler}>
-                {
-                    props.fold ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />
-                }
-            </Button>
-            <span className='ch_title'>Welcome To Gavin React Template</span>
+            <div>
+                <Button type='text' onClick={props.foldHandler}>
+                    {
+                        props.fold ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />
+                    }
+                </Button>
+                <span className='ch_title'>Welcome To Gavin React Template</span>
+            </div>
+            <div>
+                <Popover content={<UserInfoPopoverContent />}>
+                    <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>G</Avatar>
+                </Popover>
+            </div>
         </Header>
     );
 }
